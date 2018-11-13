1068050658ba574b14972074b45ddb9c35fccaad
Val++le <v.kummert@drmnt.de>
2018-11-13 14:34:34 +0100

    bugfix new repo

--- add.php
+++ add.php
@@ -3,15 +3,17 @@
 if ( $_POST['link'] <> "" )
 {
 
-			$handle = fopen ( "gitlinks.txt", "a" );		
-		   	//write from form in stream
-			fwrite ( $handle, $_POST['link']);
-			//close stream
+		$handle = fopen ( "gitlinks.txt", "a" );		
+		//write from form in stream
+		fwrite ( $handle, $_POST['link']);
+		fwrite ( $handle, PHP_EOL);
+		//close stream
     		fclose ( $handle );	
     		$handle = fopen ( "deploypath.txt", "a" );		
-		   	//write from form in stream
-			fwrite ( $handle, $_POST['directory']);
-			//close stream
+		//write from form in stream
+		fwrite ( $handle, $_POST['directory']);
+		fwrite ( $handle, PHP_EOL);
+		//close stream
     		fclose ( $handle );	
 
     echo '<h1>Inhalt gespeichert</h1>';
