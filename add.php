52773586b41a4ecd7d6e7b053e6560acc79303ec
admin <blank@h2808199.stratoserver.net>
2019-01-21 10:39:59 +0100

    beautify

--- add.php
+++ add.php
@@ -3,18 +3,18 @@
 if ( $_POST['link'] <> "" )
 {
 
-		$handle = fopen ( "gitlinks.txt", "a" );		
-	   	//write from form in stream
-		fwrite ( $handle, $_POST['link']);
-		fwrite ( $handle, PHP_EOL);
-		//close stream
-    		fclose ( $handle );	
-    		$handle = fopen ( "deploypath.txt", "a" );		
-	   	//write from form in stream
-		fwrite ( $handle, $_POST['directory']);
-		fwrite ( $handle, PHP_EOL);
-		//close stream
-    		fclose ( $handle );	
+	$handle = fopen ( "gitlinks.txt", "a" );		
+   	//write from form in stream
+	fwrite ( $handle, $_POST['link']);
+	fwrite ( $handle, PHP_EOL);
+	//close stream
+	fclose ( $handle );	
+	$handle = fopen ( "deploypath.txt", "a" );		
+   	//write from form in stream
+	fwrite ( $handle, $_POST['directory']);
+	fwrite ( $handle, PHP_EOL);
+	//close stream
+	fclose ( $handle );	
 
     header("Location: index.php");
     // Datei wird nicht weiter ausgef?hrt
